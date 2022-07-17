f98f08d934a687c21dbe5da0298c2dcc283fd577
Jssalberto <jssalbertojimenez500@gmail.com>
2022-07-17 01:57:38 -0500

    formulario renta, venta y button imprimir document

---
+++ Models/fastAPI.py
@@ -0,0 +1,6 @@
+class FastAPI_app():
+    def __init__(self,cuenta, fecha, edad, ciudad):
+        self.cuenta = cuenta
+        self.fecha = fecha
+        self.edad = edad
+        self.ciudad = ciudad
--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from flask import redirect
 
 from Models.renta import Renta
 from Models.venta import Venta
+from Models.fastAPI import FastAPI_app
 
 import pyrebase
 #CADENA DE CONEXION
@@ -30,86 +31,164 @@ app = Flask(__name__)
 def login():
     return render_template('login.html')
 
+
 @app.route('/inicio', methods=['GET'])
 def inicio():
     return render_template('index.html')
+
 
 @app.route('/estructura', methods=['GET'])
 def estructura():
     return render_template('estructura.html')
 
+#!===========================================Formnt========================================================
+#!===========================================Formnt========================================================
+
+@app.route('/FastAPI', methods=['GET'])
+def FastAPI():
+    Lista_valores = db.child("plan_Venta_FastAPI").get().val()
+    try:
+        return render_template("FastAPI.html",Lista_valores  = Lista_valores.values())
+    except:
+        return render_template('FastAPI.html')
+
+@app.route('/FormFastAPI')
+def FormFastAPI():
+    return render_template('FormFastAPI.html')
+
+@app.route('/save_data_fastAPI', methods = ['POST'])
+def save_data_fastAPI():
+    cuenta = request.form.get('cuenta')
+    fecha = request.form.get('fecha')
+    edad = request.form.get('edad')
+    ciudad = request.form.get('ciudad')
+    nueva_venta = FastAPI_app(cuenta, fecha, edad, ciudad)
+    enviar_respuesta_venta = json.dumps(nueva_venta.__dict__)
+    crear_formato_venta = json.loads(enviar_respuesta_venta)
+    db.child("plan_Venta_FastAPI").push(crear_formato_venta)
+    return redirect(url_for('FastAPI'))
 
 #*=======================================================================================
 #*=======================================================================================
-# ______________ Generar Contrato plan venta______________
-@app.route('/planventa', methods=['GET'])
-def planventa():
-    # db.child("plan_Venta").push({})
+
+# @app.route('/tableregistrorenta', methods=['GET'])
+# def tableregistrorenta():
+#     return render_template('tableregistrorenta.html')
+
+    
+#!===========================================RECIBO========================================================
+#!===========================================RECIBO========================================================
+
+@app.route('/documentrecibo', methods=['GET'])
+def documentrecibo():
+    return render_template('documentrecibo.html')
+
+@app.route('/formrecibo', methods=['GET'])
+def formrecibo():
+    return render_template('formrecibo.html')
+
+
+#!============================================VENTA==============================================================
+#!============================================VENTA==============================================================
+@app.route('/documentventa', methods=['GET'])
+def documentventa():
+    return render_template('documentventa.html')
+
+@app.route('/formplanventa', methods=['GET'])
+def formplanventa():
+    return render_template('formplanventa.html')
+
+#_______registrosVenta
+@app.route('/tableregistroventa', methods=['GET'])
+def tableregistroventa():
     lista_registros_venta = db.child("plan_Venta").get().val()
     try:
         lista_indice_venta = lista_registros_venta.keys()
-        #lista_indice_final_venta = list(lista_indice_venta )
-        return render_template('planventa.html',elemento_registros_venta =  lista_registros_venta.values(),  )
+        lista_indice_final_venta = list(lista_indice_venta)
+        return render_template('tableregistroventa.html', elemento_registros_venta=lista_registros_venta.values(), lista_indice_final_venta = lista_indice_final_venta)
     except:
-        return render_template('planventa.html')
+        return render_template('tableregistroventa.html')
+    
 
 @app.route('/save_data_venta', methods=['POST'])
 def save_data_venta():
     cuenta = request.form.get('cuenta')
     cuotamensual = request.form.get('cuotamensual')
     nombrecliente = request.form.get('nombrecliente')
-    domicilio  = request.form.get('domicilio ')
+    domicilio  = request.form.get('domicilio')
     fecha = request.form.get('fecha')
     persona = request.form.get('persona')
     ciudad = request.form.get('ciudad')
     precio = request.form.get('precio')
     equipos = request.form.get('equipos')
+    
     nueva_venta = Venta(cuenta, cuotamensual, nombrecliente, domicilio, fecha, persona, ciudad, precio, equipos)
     enviar_respuesta_venta = json.dumps(nueva_venta.__dict__)
     crear_formato_venta = json.loads(enviar_respuesta_venta)
     db.child("plan_Venta").push(crear_formato_venta)
-    return redirect(url_for('inicio'))
-#*=======================================================================================
-#*=======================================================================================
-# ______________ Generar Contrato plan renta ______________
-@app.route('/planrenta', methods=['GET'])
-def planrenta():
-    # db.child("plan_Renta").push({"asfsfaf":"asfasfaf"})
-    lista_registros_renta = db.child("plan_Renta").get().val()
-    try:
-        # lista_indice_renta = lista_registros_renta.keys()
-        #lista_indice_final_renta = list(lista_indice_renta )
-        return render_template('planrenta.html',elemento_registros_renta =  lista_registros_renta.values(),  )
-    except:
-        return render_template('docrenta.html')
+    return redirect(url_for('tableregistroventa'))
 
+@app.route('/eliminar_registro_venta', methods=["GET"])
+def eliminar_registro_venta():
+    id = request.args.get("id")#AGREGAR SOLO ESTO
+    db.child("plan_Venta").child(str(id)).remove()
+    return redirect(url_for('tableregistroventa'))
+
+#*==========================================RENTA===================================================================
+#*==========================================RENTA===================================================================
+@app.route('/formplanrenta', methods=['GET'])
+def formplanrenta():
+    return render_template('formplanrenta.html')
+
+@app.route('/documentrenta', methods=['GET'])
+def documentrenta():
+    return render_template('documentrenta.html')
+
+@app.route('/tableregistrorenta', methods=['GET'])
+def tableregistrorenta():
+    lista_registros_renta=db.child("plan_Renta").get().val()
+    try:
+        lista_indice_renta = lista_registros_renta.keys()                  
+        lista_indice_final_renta = list(lista_indice_renta)
+        return render_template('tableregistrorenta.html', registros_renta=lista_registros_renta.values(), lista_indice_final_renta=lista_indice_final_renta)
+    except:
+        return render_template('tableregistrorenta.html')
+
+@app.route('/imprimir_document_renta', methods=["GET"])
+def imprimir_document_renta():
+    id  = request.args.get("id")
+    db.child("plan_Renta").child(str(id)).get()
+    return render_template('documentrenta.html')
+
+    
 @app.route('/save_data_renta', methods=['POST'])
 def save_data_renta():
     cuenta = request.form.get('cuenta')
     cuotamensual = request.form.get('cuotamensual')
     nombrecliente = request.form.get('nombrecliente')
-    domicilio = request.form.get('domicilio')
+    domicilio  = request.form.get('domicilio')
     fecha = request.form.get('fecha')
     persona = request.form.get('persona')
     ciudad = request.form.get('ciudad')
     precio = request.form.get('precio')
     equipos = request.form.get('equipos')
+    
     nueva_renta = Renta(cuenta, cuotamensual, nombrecliente, domicilio, fecha, persona, ciudad, precio, equipos)
     enviar_respuesta_renta = json.dumps(nueva_renta.__dict__)
     crear_formato_renta = json.loads(enviar_respuesta_renta)
     db.child("plan_Renta").push(crear_formato_renta)
-    return redirect(url_for('docrenta'))
-    # return render_template('docrenta.html')
+    return redirect(url_for('tableregistrorenta'))
+
+@app.route('/eliminar_registro_renta', methods=["GET"])
+def eliminar_registro_renta():
+    id = request.args.get("id")#AGREGAR SOLO ESTO
+    db.child("plan_Renta").child(str(id)).remove()
+    return redirect(url_for('tableregistrorenta'))
+
+
 
 #*=======================================================================================
 #*=======================================================================================
-@app.route('/docrenta', methods=['GET'])
-def docrenta():
-    return render_template('docrenta.html')
-
-@app.route('/docventa', methods=['GET'])
-def docventa():
-    return render_template('docventa.html')
 # ______________ Contratos Generales ______________
 
 @app.route('/contratoGeneral', methods=['GET'])
@@ -132,9 +211,7 @@ def cancelados():
 
 # ______________ Facturación ______________
 
-@app.route('/recibo', methods=['GET'])
-def recibo():
-    return render_template('login.html')
+
 
 if __name__=='__main__':
     app.run(debug = True)
