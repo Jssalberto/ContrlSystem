41f8e09f900fa597901fc8ed566218be2e444b6b
jared27gomez.agg@gmail.com <adal13gomez.agg@gmail.com>
2022-07-18 07:57:39 -0500

    Session

---
+++ static/js/sesion.js
@@ -0,0 +1,12 @@
+
+		// function validar()
+		// {
+        //     var usuario = document.getElementById("inputEmail").value;
+        //     var Contraseña = document.getElementById("inputPassword").value;	
+
+        //     if (usuario == "adal" != Contraseña == "1234") {
+        //         window.location.href = "{{ url_for('login') }}"
+        //     }else{
+        //         window.location.href = "{{ url_for('inicio') }}"
+		// 	}
+		// }
